77721e7080108c959270ece2cba111fa401596e9
Souvik Guha <svkguha123@gmail.com>
2023-04-28 11:51:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ const apiUrls = [
 ];
 
 // You can write your code here
+function fetchData(url) {
+  return fetch(url).then((response) => response.json());
+}
+
+// Use Promise.all() to fetch data from all APIs and measure the time taken
+const startAll = performance.now();
+Promise.all(apiUrls.map(fetchData))
+  .then((results) => {
+    const endAll = performance.now();
+    const timeTakenAll = (endAll - startAll) / 1000;
+    document.getElementById("output-all").innerText = timeTakenAll.toFixed(3);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Use Promise.any() to fetch data from APIs and measure the time taken
+const startAny = performance.now();
+Promise.any(apiUrls.map(fetchData))
+  .then((result) => {
+    const endAny = performance.now();
+    const timeTakenAny = (endAny - startAny) / 1000;
+    document.getElementById("output-any").innerText = timeTakenAny.toFixed(3);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
